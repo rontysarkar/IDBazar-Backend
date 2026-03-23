69163283b5a9195101c6ab8716c5ddb64071fd82
rontysarkar <rontysarkar2023@gmail.com>
2026-03-24 00:59:37 +0600

    add user models

---
+++ config/models/User.ts
@@ -0,0 +1,53 @@
+import mongoose, {
+  Document,
+  Schema,
+} from "mongoose";
+import bcrypt from "bcrypt";
+
+export interface IUSER extends Document {
+  name: string;
+  email: string;
+  password?: string;
+  googleId?: string;
+  profilePicture?: string;
+  phoneNumber?: string;
+  isVarified: boolean;
+  varificationToken?: string;
+  resetPasswordToken?: string;
+  resetPasswordExpires?: Date;
+  agreeTerms: boolean;
+  comparePassword(candidatePassword: string): Promise<boolean>;
+}
+
+const userSchema = new Schema<IUSER>({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String },
+  googleId: { type: String },
+  profilePicture: { type: String, default: null },
+  phoneNumber: { type: String, default: null },
+  isVarified: { type: Boolean, default: false },
+  agreeTerms: { type: Boolean, default: false },
+  varificationToken: { type: String, default: null },
+  resetPasswordToken: { type: String, default: null },
+  resetPasswordExpires: { type: String, default: null },
+},{timestamps:true});
+
+userSchema.pre("save", async function (this: IUSER) {
+  if (!this.isModified("password")) return;
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password!, salt);
+  } catch (error: any) {
+    throw error;
+  }
+});
+
+userSchema.methods.comparePassword = async function (
+  candidatePassword: string,
+): Promise<boolean> {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+export default mongoose.model<IUSER>("User", userSchema);
